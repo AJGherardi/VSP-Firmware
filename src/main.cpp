9fd3153a6ad9c8c65650d83b6a6c197bcba48041
alexander gherardi <alexander.2003@live.com>
2019-04-26 16:19:42 -0400

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo thrustServo;
+Servo pitchServo;
+
+int dcSpeed = 3;
+int dcDir = 6;
+
+int posThrust = 0;
+int posPitch = 0;
+
+void setup() {
+   pinMode(dcSpeed,OUTPUT);
+   pinMode(dcDir,OUTPUT);
+}
+
+void loop() {
+   digitalWrite(dcDir,HIGH);
+   for(size_t i = 60; i < 255; i++)
+   {
+      analogWrite(dcSpeed,i);
+      delay(50);
+   }
+   
+   analogWrite(dcSpeed,255);   
+   delay(5000);
+   for(size_t i = 255; i > 60; i--)
+   {
+      analogWrite(dcSpeed,i);
+      delay(50);   
+   }
+   delay(5000);
+}
